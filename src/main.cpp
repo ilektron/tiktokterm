f0589790ad1360f5ed6958a9cc513e03091f10e7
Stephen Farnsworth <robotoman@gmail.com>
2020-03-15 12:26:54 -0600

    Switch http lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "../lib/cpp-httplib/httplib.h"
+
 int main(int argc, const char* argv[]) {
-  std::cout << "Hola mondo" << std::endl;
+  try
+  {
+    httplib::Client cli("www.tiktok.com");
+    auto res = cli.Get("/@steveplusplus");
+    if (res && res->status == 200) {
+      std::cout << res->body << std::endl;
+    } else {
+      std::cout << "Status: " << res << std::endl;
+    }
+
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Request failed, error: " << e.what() << std::endl;
+  }
 }
+
+
